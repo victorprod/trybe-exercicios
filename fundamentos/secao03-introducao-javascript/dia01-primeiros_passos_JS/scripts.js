3486d2a9a54a0466a0d5797b0d3961c5d75fd15d
victorprod <victorufu@hotmail.com>
2023-01-24 01:28:58 -0300

    exercicio1

---
+++ fundamentos/secao03-introducao-javascript/dia01-primeiros_passos_JS/scripts.js
@@ -0,0 +1,18 @@
+/*const a = 8
+const b = 6
+if (a>b) {
+    console.log("O maior número é:", a);
+} else {
+    console.log("O maior número é:", b);
+}*/
+
+const a = 32
+const b = 29
+const c = 21
+if (a>b && a>c) {
+    console.log("O maior número é:", a);
+} if (b>a && b>c) {
+    console.log("O maior número é:", b);
+} else {
+    console.log("O maior número é:", c);
+}
